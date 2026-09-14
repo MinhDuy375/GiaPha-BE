c3935d717065f8d84a390118cda410e12826ea80
MinhDuy375 <minhduy37a@gmail.com>
2026-09-15 01:08:43 +0700

    Revert "fix  url connect to BE"
    This reverts commit bf2b6bb1e134bc6ae905fb72a9fb9c7b4c7a2025.

--- LacVietGenealogy.API/Program.cs
+++ LacVietGenealogy.API/Program.cs
@@ -109,15 +109,11 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddSingleton<IAuthorizationHandler, PermissionHandler>();
 
 // CORS Config
-var allowedOrigins = builder.Configuration
-    .GetSection("Cors:AllowedOrigins")
-    .Get<string[]>() ?? Array.Empty<string>();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins(allowedOrigins)
+            .WithOrigins("http://localhost:5173", "http://localhost:5174")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials());
